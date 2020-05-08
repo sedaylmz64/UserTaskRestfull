2e16c7ecf0ab510feec110f7c64887cdf999f4e6
Seda <sedaylmz64@gmail.com>
2020-05-08 19:22:03 +0300

    code refactor

--- src/main/java/com/example/deneme/DenemeApplication.java
+++ src/main/java/com/example/deneme/DenemeApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
 @EnableScheduling
 public class DenemeApplication{
 
